5ab0b68610e5c9cb7257a2f7255378c30d9040bb
Huawei <Huawei@LAPTOP-5LRHA01L>
2022-10-02 19:33:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Mainclass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Конечное число равно 546. Найти исходное.");
+        Console.WriteLine("Начало расчетов...");
+        string a = "5"; // Число сотен (Первая цифра)
+        string b = "4"; // Число десятков (Вторая цифра)
+        string c = "6"; // Число единиц (Третья цифра)
+        string[] n0 = { b, a, c }; // По условию, вторая цифра (b) меняется местами с первой цифрой (a)
+        string x = string.Join(null, n0);
+        Console.WriteLine($"Начальное число равно {x}");
+    }
+}
